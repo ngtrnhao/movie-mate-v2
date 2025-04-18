dfca44a5ea3442ecc49effa39c874f729ad01491
Nguyen Truong Nhat Hao <nguyentruongnhathao1922@gmail.com>
2025-04-18 14:11:00 +0700

    fix: update Header import path in App component
    - Changed the import statement for the Header component to use the index file for better modularity and clarity in the file structure.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import Header from './components/Header';
+import Header from './components/Header/index';
 import { ThemeProvider } from './context/ThemeContext';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from './components/footer';

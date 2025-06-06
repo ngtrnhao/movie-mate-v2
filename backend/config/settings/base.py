e743c5ee37792741426a738e9e2d6c13534e5f65
Nguyen Truong Nhat Hao <nguyentruongnhathao1922@gmail.com>
2025-06-06 16:57:01 +0700

    chore: Refactor ALLOWED_HOSTS and add MIGRATION_SECRET_KEY
    - Updated the ALLOWED_HOSTS setting in base.py to include specific domains for production, enhancing security and deployment configuration.
    - Removed the ALLOWED_HOSTS entry from render.yaml to streamline environment variable management.
    - Added MIGRATION_SECRET_KEY to render.yaml for secure migration operations, aligning with recent security enhancements.
    
    These changes improve the clarity and security of environment configurations, ensuring proper deployment and migration management.

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -16,7 +16,12 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = [
+    'movie-mate-v2.onrender.com',
+    'localhost',
+    '127.0.0.1',
+    '.onrender.com',  # This will allow all subdomains of onrender.com
+]
 
 # Application definition
 INSTALLED_APPS = [
